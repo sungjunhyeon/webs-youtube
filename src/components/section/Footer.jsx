7f02131c50504359bf3ed2ab25260dc2dc1ebd3a
Sungjun Hyeon <wnsgus880@naver.com>
2024-08-12 07:12:49 +0900

    메인 콘텐츠 작업

--- src/components/section/Footer.jsx
+++ src/components/section/Footer.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer id='footer' role='contentinfo'>
-        Footer
+         <a href='mailto:webstoryboy@naver.com' rel='noopener noreferrer'>
+                Sung JunHyeon@naver.com
+            </a>
     </footer>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,12 @@
 import React from 'react'
 import Main from '../components/section/Main'
+import Today from '../components/contents/Today'
+import Developer from '../components/contents/Developer'
+import Webd from '../components/contents/Webd'
+import Website from '../components/contents/Website'
+import Gsap from '../components/contents/Gsap'
+import Portfolio from '../components/contents/Portfolio'
+import Youtube from '../components/contents/Youtube'
 
 const Home = () => {
     return (
@@ -7,7 +14,13 @@ const Home = () => {
             title="SungJunHyeon 유튜브 채널" 
             description="유튜브 채널 페이지입니다."
         >
-            Home
+            <Today />
+            <Developer />
+            <Webd />
+            <Website />
+            <Gsap />
+            <Portfolio />
+            <Youtube />
         </Main>
     )
 }
