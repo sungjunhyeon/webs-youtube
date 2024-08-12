bd58bffeee74d6294f3bf7f220a8f713c0f74fe4
Sungjun Hyeon <wnsgus880@naver.com>
2024-08-12 09:41:24 +0900

    메인 섹션 나머지 콘텐츠 작업

--- src/pages/Gsap.jsx
+++ src/pages/Gsap.jsx
@@ -1,12 +1,28 @@
 import React from 'react'
 import Main from '../components/section/Main'
 
+import { gsapText } from '../data/gsap'
+import { Link } from 'react-router-dom'
+
 const Gsap = () => {
     return (
         <Main 
             title = "GSAP 사이트"
             description="GSAP 사이트 튜토리얼 강의입니다.">
-            Gsap
+            <section id='gsap'>
+            <h2>🤓 GSAP 패럴랙스 효과를 하고 싶다면!</h2>
+                <div className='video__inner'>
+                    {gsapText.map((video, key) => (
+                        <div className="video" key={key}>
+                            <div className="video__thumb play__icon">
+                                <Link to={`/video/${video.videoId}`}>
+                                    <img src={video.img} alt={video.title} />
+                                </Link>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </section>
         </Main>
     )
 }
--- src/pages/Port.jsx
+++ src/pages/Port.jsx
@@ -1,12 +1,27 @@
 import React from 'react'
 import Main from '../components/section/Main'
 
+import { portfolioText } from '../data/portfolio'
+import { Link } from 'react-router-dom'
 const Port = () => {
     return (
         <Main 
             title = "포트폴리오 사이트"
             description="포트폴리오 사이트 튜토리얼 강의입니다.">
-            Port
+                <section id='portfolio'>
+                <h2>🤗 포트폴리오 만드는 방법을 공유합니다.</h2>
+                    <div className='video__inner'>
+                        {portfolioText.map((video, key) => (
+                            <div className="video" key={key}>
+                                <div className="video__thumb play__icon">
+                                    <Link to={`/video/${video.videoId}`}>
+                                        <img src={video.img} alt={video.title} />
+                                    </Link>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                </section>
         </Main>
     )
 }
